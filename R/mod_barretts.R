dfab0b542f38ef0b6b940023176b154f8e88f544
Chris Beeley <chris.beeley@gmail.com>
2021-02-08 16:03:39 +0000

    Add more Barrett's outputs

--- R/mod_barretts.R
+++ R/mod_barretts.R
@@ -41,8 +41,8 @@ mod_barretts_server <- function(id, merge_data, map_terms){
                                                        map_terms()$Map_Findings2In)
       
       barretts_data2 <- EndoMineR::Barretts_PragueScore(barretts_data, 
-                                                       "findings", 
-                                                       NULL)
+                                                        "findings", 
+                                                        NULL)
       barretts_data$mytext <- NULL
       barretts_data$MStage <- as.numeric(barretts_data$MStage)
       barretts_data$CStage <- as.numeric(barretts_data$CStage)
@@ -66,6 +66,48 @@ mod_barretts_server <- function(id, merge_data, map_terms){
           ggplot2::geom_histogram(stat = "count") +
           ggplot2::theme(axis.text.x = ggplot2::element_text(angle = -90))
       )
+    })
+    
+    output$plotBarrEQ <- plotly::renderPlotly({
+      
+      Hiatus <- merge_data() %>% 
+        dplyr::group_by(!! rlang::sym(map_terms()$Map_EndoscopistIn)) %>% 
+        dplyr::summarise(Hiatus = (sum(grepl("[Hh]iatus|[Ii]sland", 
+                                      !!rlang::sym(map_terms()$Map_FindingsIn))) / dplyr::n()) * 100)
+      Island <- merge_data() %>% 
+        dplyr::group_by(!! rlang::sym(map_terms()$Map_EndoscopistIn)) %>% 
+        dplyr::summarise(Island = (sum(grepl("[Ii]sland", 
+                                      !!rlang::sym(map_terms()$Map_FindingsIn))) / dplyr::n()) * 100)
+      Pinch <- merge_data() %>% 
+        dplyr::group_by(!! rlang::sym(map_terms()$Map_EndoscopistIn)) %>% 
+        dplyr::summarise(Pinch = (sum(grepl("[Pp]inch", 
+                                     !!rlang::sym(map_terms()$Map_FindingsIn))) / dplyr::n()) * 100)
+      Lesion <- merge_data() %>% 
+        dplyr::group_by(!! rlang::sym(map_terms()$Map_EndoscopistIn)) %>% 
+        dplyr::summarise(Lesion = (sum(grepl("esion|odule|lcer", 
+                                      !!rlang::sym(map_terms()$Map_FindingsIn))) / dplyr::n()) * 100)
+      
+      FinalTable <- dplyr::full_join(Hiatus, Island, by = map_terms()$Map_EndoscopistIn)
+      FinalTable <- dplyr::full_join(FinalTable, Pinch, by = map_terms()$Map_EndoscopistIn)
+      FinalTable <- dplyr::full_join(FinalTable, Lesion, by = map_terms()$Map_EndoscopistIn)
+      
+      FinalTable <- data.frame(FinalTable)
+      
+      #Need to gather the table to make tidy for ggplot
+      
+      FinalTable <- tidyr::gather(FinalTable,
+                                  key = "DocumentedElement",
+                                  value = "PercentDocs",
+                                  -!!rlang::sym(map_terms()$Map_EndoscopistIn))
+      
+      key <- map_terms()$Map_EndoscopistIn
+      
+      p <- ggplot2::ggplot(FinalTable, 
+                           ggplot2::aes_string(x = key, y = "PercentDocs", fill = "DocumentedElement")) + 
+        ggplot2::geom_bar(stat = "identity") + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = -90))
+      
+      plotly::ggplotly(p, source = "subset", key = key) %>% 
+        plotly::layout(dragmode = "select")
     })
     
     output$test <- DT::renderDT({
