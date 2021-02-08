b3e314fec0e55aa925d77d2beb65af657b6db824
Chris Beeley <chris.beeley@gmail.com>
2021-02-08 17:44:43 +0000

    Polyps tab complete, but with errors

--- R/mod_polyps.R
+++ R/mod_polyps.R
@@ -12,11 +12,12 @@ mod_polyps_ui <- function(id){
   tagList(
     fluidRow(
       column(5,
-             plotly::plotlyOutput(ns("plotPolypEQ"))
+             plotly::plotlyOutput(ns("plotPolypEQ")),
+             plotly::plotlyOutput(ns("endoscopyUse_EndoscopyUsePolyp"))
       ),
       column(7,
              DT::DTOutput(ns("grs_table")),
-             plotly::plotlyOutput(ns("endoscopyUse_EndoscopyUsePolyp"))
+             DT::DTOutput(ns("drilldown"))
       )
     )
   )
@@ -64,6 +65,12 @@ mod_polyps_server <- function(id, merge_data, map_terms){
       ForGRS
     })
     
+    reduce_polyp <- reactive({
+      
+      polypdata <- merge_data()[Reduce(`|`, lapply(merge_data(), grepl, pattern = "polyp")),]
+      polypdata <- polypdata[Reduce(`|`, lapply(polypdata, grepl, pattern = "colonoscopy")),]
+    })
+    
     output$plotPolypEQ <- plotly::renderPlotly({
       
       MyPolypTable <- tidyr::gather(
@@ -99,5 +106,99 @@ mod_polyps_server <- function(id, merge_data, map_terms){
       dom = 'Bfrtip',
       buttons = c('copy', 'csv', 'excel', 'pdf', 'print','colvis'))
     )
+    
+    output$endoscopyUse_EndoscopyUsePolyp <- plotly::renderPlotly({
+      
+      dtData <- reduce_polyp() %>% 
+        dplyr::group_by(!!rlang::sym(map_terms()$Map_EndoscopyDateIn)) %>% 
+        dplyr::summarise(n = dplyr::n())
+      
+      #Get rid of NA's as they mess things up.
+      
+      dtData <- na.omit(data.table::as.data.table(dtData))
+      
+      p1 = ggTimeSeries::ggplot_calendar_heatmap(
+        dtData,
+        map_terms()$Map_EndoscopyDateIn,
+        'n'
+      )
+      
+      # adding some formatting
+      p1 + 
+        ggplot2::xlab('') + 
+        ggplot2::ylab('') + 
+        ggplot2::scale_fill_continuous(low = 'green', high = 'red') + 
+        ggplot2::facet_wrap(~ Year, ncol = 1)
+    })
+    
+    drilldataPolyp <- reactive({
+      shiny::validate(
+        need(length(input$grs_table_rows_selected) > 0, "Select rows to drill down!")
+      )
+      
+      selected_species <- polyp_data()[input$grs_table_rows_selected, ]
+      variables <- c(t(as.character(selected_species[, 1])))
+      mycolname <- colnames(selected_species)[1]
+      df <- reduce_polyp()[reduce_polyp()[, mycolname] %in% variables, ]
+      
+      df %>%
+        dplyr::select(map_terms()$Map_HospitalNumberIn, 
+               map_terms()$Map_EndoscopyDateIn,
+               map_terms()$Map_FindingsIn, 
+               map_terms()$Map_MicroscopicTextIn, 
+               dplyr::contains("url"))
+    })
+    
+    
+    output$drilldown <- DT::renderDT({
+      
+      DT::datatable(
+        drilldataPolyp(),
+        escape = F, 
+        extensions = c("Select", "Buttons"), 
+        selection = "none",
+        callback = DT::JS(
+          "var ncols = table.columns().count();",
+          "var tbl = table.table().node();",
+          "var tblID = $(tbl).closest('.datatables').attr('id');",
+          "table.on('click', 'tbody td', function(){",
+          "  // if the column is selected, deselect it:",
+          "  if(table.column(this, {selected: true}).length){",
+          "    table.column(this).deselect();",
+          "  // otherwise, select the column unless it's among the last two columns:",
+          "  } else if([ncols-1, ncols-2].indexOf(table.column(this).index()) === -1){",
+          "    table.column(this).select();",
+          "  }",
+          "  // send selected columns to Shiny",
+          "  var indexes = table.columns({selected:true}).indexes();",
+          "  var indices = Array(indexes.length);",
+          "  for(var i = 0; i < indices.length; ++i){",
+          "    indices[i] = indexes[i];",
+          "  }",
+          "  Shiny.setInputValue(tblID + '_columns_selected', indices);",
+          " var checkboxes = document.getElementsByName('row_selected');",
+          "  var checkboxesChecked = [];",
+          " for (var i=0; i<checkboxes.length; i++) {",
+          "    if (checkboxes[i].checked) {",
+          "   checkboxesChecked.push(checkboxes[i].value);",
+          "    }",
+          "   }",
+          " Shiny.onInputChange('checked_rows',checkboxesChecked);",
+          "});"), 
+        
+        options = list(
+          columnDefs = list(
+            list(targets = as.numeric(
+              which(names(drilldataPolyp()) == names(drilldataPolyp()[map_terms()$Map_EndoscopyDateIn]))
+            ), 
+            visible = TRUE)),
+          fixedHeader = TRUE,
+          scrollX = TRUE,
+          scrollY = TRUE,
+          pageLength = 5,
+          dom = 'Bfrtip',
+          buttons = c('copy', 'csv', 'excel', 'pdf', 'print','colvis'))
+      )
+    }, server = FALSE)
   })
 }
