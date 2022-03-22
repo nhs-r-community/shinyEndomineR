1550ad7cb2765c336be3612d280540e0658f9f0f
Chris Beeley <chris.beeley@gmail.com>
2022-03-22 17:54:11 +0000

    Place entire UI in app_server.R

--- R/app_server.R
+++ R/app_server.R
@@ -6,6 +6,65 @@
 #' @noRd
 app_server <- function( input, output, session ) {
   
+  # draw interface
+  
+  output$interfaceUI <- renderUI({
+    
+    # List the first level UI elements here 
+    dashboardPage(
+      dashboardHeader(title = "Shiny Endominer"),
+      dashboardSidebar(
+        sidebarMenu(
+          menuItem("Endoscopy data", tabName = "endoData", icon = icon("user-md")),
+          menuItem("Pathology data", tabName = "pathData", icon = icon("microscope")),
+          menuItem("Merge data", tabName = "mergeData", icon = icon("object-group")),
+          menuItem("Map terms", tabName = "mapTerms", icon = icon("map")),
+          menuItem("Barrett's", tabName = "barretts", icon = icon("chart-bar")),
+          menuItem("Polyps", tabName = "polyps", icon = icon("chart-bar")),
+          menuItem("Per endoscopist", tabName = "endoscopist", icon = icon("users")),
+          menuItem("Custom", tabName = "custom", icon = icon("question-circle")),
+          downloadButton("downloadData", "Download data"),
+          fileInput("loadData", "Load data from a previous run")
+        )),
+      dashboardBody(
+        tabItems(
+          # First tab content
+          tabItem(tabName = "endoData",
+                  mod_clean_and_merge_ui("clean_and_merge_ui_1")
+          ),
+          
+          # Second tab content
+          tabItem(tabName = "pathData",
+                  mod_clean_and_merge_ui("clean_and_merge_ui_2")
+          ),
+          
+          tabItem(tabName = "mergeData",
+                  mod_merge_data_ui("merge_data_ui_1")
+          ),
+          
+          tabItem(tabName = "mapTerms",
+                  mod_map_terms_ui("map_terms_ui_1")
+          ),
+          
+          tabItem(tabName = "barretts",
+                  mod_barretts_ui("barretts_ui_1")
+          ),
+          tabItem(tabName = "polyps",
+                  mod_polyps_ui("polyps_ui_1")
+          ),
+          tabItem(tabName = "endoscopist",
+                  mod_per_endoscopist_ui("per_endoscopist_ui_1")
+          ),
+          tabItem(tabName = "custom",
+                  mod_custom_ui("custom_ui_1")
+          )
+        )
+      )
+    )
+    
+    
+  })
+  
   # initialise petit r
   
   r <- reactiveValues()
@@ -39,26 +98,26 @@ app_server <- function( input, output, session ) {
   
   # List the first level callModules here
   
-  mod_clean_and_merge_server("clean_and_merge_ui_1", 
+  mod_clean_and_merge_server("clean_and_merge_ui_1",
                              header_filename = "endo.rda",
                              r = r)
   
-  mod_clean_and_merge_server("clean_and_merge_ui_2", 
+  mod_clean_and_merge_server("clean_and_merge_ui_2",
                              header_filename = "path.rda",
                              r = r)
   
-  mod_merge_data_server("merge_data_ui_1", 
+  mod_merge_data_server("merge_data_ui_1",
                         load_prev = load_prev,
                         r = r)
   
   mod_map_terms_server("map_terms_ui_1", r = r)
   
   barretts_data <- mod_barretts_server("barretts_ui_1", r = r)
-
+  
   polyp_data <- mod_polyps_server("polyps_ui_1", r = r)
-
+  
   mod_per_endoscopist_server("per_endoscopist_ui_1", barretts_data = barretts_data,
                              polyp_data = polyp_data, r = r)
-
+  
   mod_custom_server("custom_ui_1", r = r)
 }
