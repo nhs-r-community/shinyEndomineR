bd445d5796c8fe5ec687a3872e0bbb4178bc28e8
Chris Beeley <chris.beeley@gmail.com>
2022-03-22 18:04:47 +0000

    Split UI into header, sidebar, and body

--- R/app_server.R
+++ R/app_server.R
@@ -10,59 +10,6 @@ app_server <- function( input, output, session ) {
   
   output$interfaceUI <- renderUI({
     
-    # List the first level UI elements here 
-    dashboardPage(
-      dashboardHeader(title = "Shiny Endominer"),
-      dashboardSidebar(
-        sidebarMenu(
-          menuItem("Endoscopy data", tabName = "endoData", icon = icon("user-md")),
-          menuItem("Pathology data", tabName = "pathData", icon = icon("microscope")),
-          menuItem("Merge data", tabName = "mergeData", icon = icon("object-group")),
-          menuItem("Map terms", tabName = "mapTerms", icon = icon("map")),
-          menuItem("Barrett's", tabName = "barretts", icon = icon("chart-bar")),
-          menuItem("Polyps", tabName = "polyps", icon = icon("chart-bar")),
-          menuItem("Per endoscopist", tabName = "endoscopist", icon = icon("users")),
-          menuItem("Custom", tabName = "custom", icon = icon("question-circle")),
-          downloadButton("downloadData", "Download data"),
-          fileInput("loadData", "Load data from a previous run")
-        )),
-      dashboardBody(
-        tabItems(
-          # First tab content
-          tabItem(tabName = "endoData",
-                  mod_clean_and_merge_ui("clean_and_merge_ui_1")
-          ),
-          
-          # Second tab content
-          tabItem(tabName = "pathData",
-                  mod_clean_and_merge_ui("clean_and_merge_ui_2")
-          ),
-          
-          tabItem(tabName = "mergeData",
-                  mod_merge_data_ui("merge_data_ui_1")
-          ),
-          
-          tabItem(tabName = "mapTerms",
-                  mod_map_terms_ui("map_terms_ui_1")
-          ),
-          
-          tabItem(tabName = "barretts",
-                  mod_barretts_ui("barretts_ui_1")
-          ),
-          tabItem(tabName = "polyps",
-                  mod_polyps_ui("polyps_ui_1")
-          ),
-          tabItem(tabName = "endoscopist",
-                  mod_per_endoscopist_ui("per_endoscopist_ui_1")
-          ),
-          tabItem(tabName = "custom",
-                  mod_custom_ui("custom_ui_1")
-          )
-        )
-      )
-    )
-    
-    
   })
   
   # initialise petit r
